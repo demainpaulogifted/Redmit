71337ae98ac32dfc54a26cd647e4c0cebb25270c
demainpaulogifted <demainpaulogifted@outlook.com>
2026-09-26 07:00:36 +0100

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ywfkwecubx2wbh1jvufq.supabase.co'
-const supabaseKey = 'sb_publishable_jvKVND-WOKJVrU704SqnSg_IrruYSEV' // Your new publishable key
+const supabaseUrl = 'https://ywfkxecubxiwbijviwfq.supabase.co'
+const supabaseKey = 'sb_publishable_jvKVND-WOKJVrU704SqnSg_IrruYSEV'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
